3d90939502122aea1b42bdfe3c3546a1551891d1
Percy-gif <percymajor@gmail.com>
2020-09-27 03:13:44 -0500

    complete

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -16,14 +16,23 @@ class Waiter
         @@all 
     end 
 
-   def waiters
-        meals.map do |meal|
-            meal.waiter
+    def meals
+        Meal.all.select do |meal|
+          meal.waiter == self #checking for waiter now
         end
-    end
-
-    def new_meal(waiter, total, tip=0)
-        Meal.new(waiter, self, total, tip)
       end
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+      end
+
+      def best_tipper
+        best_tipped_meal = meals.max do |meal_a, meal_b|
+          meal_a.tip <=> meal_b.tip
+        end
+      
+        best_tipped_meal.customer
+      end
+    
 
 end
